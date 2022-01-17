02cbc7ee454dcd342abaf14f988295da9bacdc08
Ramiro Sandoval <ramiro_sando@outlook.com>
2022-01-17 14:19:53 -0300

    No longer hardcoded credentials. Also, scripts now take arguments instead of having to edit the code

--- addContacts.py
+++ addContacts.py
@@ -1,65 +1,108 @@
 from selenium import webdriver
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from sys import argv
+from os import environ
 import pandas as pandas
 
 
+
+#Alertas plataforma info
+url = "";
+alertasUser = environ['alertasUser'];
+alertasPassword = environ['alertasPassword'];
+#Alertas plataforma info
+
+#Lote info
+nombreDeLote = "";
+nombreDeColumnaConNombres = "";
+nombreDeColumnaConTelefonos = "";
+#Lote info
+
+#Manejo de argumentos
+if argv[1]:
+    nombreDeLote = argv[1];
+
+if argv[2]:
+    nombreDeColumnaConNombres = argv[2];
+
+if argv[3]:
+    nombreDeColumnaConTelefonos = argv[3];
+
+if argv[4]:
+    if argv[4] == 'uywenance':
+        url = 'http://uywenance.sondeosglobal.com/user/login';
+    elif argv[4] == 'arwenance':
+        url = 'http://arwenance.sondeosglobal.com/user/login';
+#Manejo de argumentos
+
+
 #Pandas
-loteExcel = pandas.read_excel('BASE UY 5.xlsx');
-columnaNombresDelLote = loteExcel['Nombre'];
-columnaNTelefonosDelLote = loteExcel['Tel WA'];
+loteExcel = pandas.read_excel(nombreDeLote);
+columnaNombresDelLote = loteExcel[nombreDeColumnaConNombres];
+columnaNTelefonosDelLote = loteExcel[nombreDeColumnaConTelefonos];
 #Pandas
 
 #Selenium
-browser = webdriver.Firefox()
-browser.get('http://uywenance.sondeosglobal.com')
+browser = webdriver.Firefox();
+browser.get(url);
 #Selenium
 
 #INICIO LOGUEO
-usernameInput = browser.find_element_by_xpath("//input[@name='_username']")
-passwordInput = browser.find_element_by_xpath("//input[@name='_password']")
+usernameInput = browser.find_element_by_xpath("//input[@name='_username']");
+passwordInput = browser.find_element_by_xpath("//input[@name='_password']");
 
-usernameInput.send_keys("admin")
-passwordInput.send_keys("admin.123")
-browser.find_element_by_xpath("//button[@type='submit']").click()
+#Consigue credenciales mediante variables de ambiente
+usernameInput.send_keys(alertasUser);
+passwordInput.send_keys(alertasPassword);
+#Consigue credenciales mediante variables de ambiente
+
+#Clickea boton de logueo
+browser.find_element_by_xpath("//button[@type='submit']").click();
 #FIN LOGUEO
 
 #Apretar boton de gestion de contactos
-browser.find_element_by_xpath("//h4[text()='Gestion de Contactos']").click()
+browser.find_element_by_xpath("//h4[text()='Gestion de Contactos']").click();
 
 #Apretar boton de añadir
-browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click()
+browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click();
 
-nombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");
-telefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");
+elementoNombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");
+elementoTelefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");
 
 counterLimit = 500;
 counter = 0;
 repetidosCounter = 0;
+counterForConsoleLog = 0;
 
-for nombre in columnaNombresDelLote:
-    nombreDeContacto.send_keys(f"{nombre}")
+print(f"Lote seleccionado: {nombreDeLote}");
 
-    while counter<counterLimit:
-        telefonoContacto.send_keys(f"{columnaNTelefonosDelLote[counter]}")
-        counter+=1;
-        break;
+for nombre in columnaNombresDelLote: #Por cada nombre en el lote
+    elementoNombreDeContacto.send_keys(f"{nombre}");#ingresa el nombre tomado en la iteracion actual
 
-    try:
-        browser.find_element_by_xpath("//input[@value='Guardar']").click();
-        browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click()
-    except NoSuchElementException:
-        repetidosCounter+=1;
-        nombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");
-        telefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");
+    while counter<counterLimit:#Y, mientras no se hayan cargado todos los contactos,
+        elementoTelefonoContacto.send_keys(f"{columnaNTelefonosDelLote[counter]}");#ingresar el telefono correspondiente al nombre tomado
+        counter+=1;#aumentar contador de informacion de contactos cargada.
+        break; # salir de la iteracion del while
 
-        nombreDeContacto.clear()
-        telefonoContacto.clear()
+    try:#Intenta:
+        browser.find_element_by_xpath("//input[@value='Guardar']").click();#clickear el boton de guardar una vez ingresada la info, si es exitoso redirige a la pagina anterior.
+        browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click();#Volver a la pagina de suma de contacto
+        counterForConsoleLog+=1;#aumentar contador de numeros cargados exitosamente
+        print(f"Se han cargado {counterForConsoleLog} numeros exitosamente!");#Mostrar en consola los contactos cargados exitosamente (valor del contador anterior).
+    except NoSuchElementException:#Si al apretar el boton de guardar se detecta que el usuario ya existia, jamas redirige a la pagina anterior, por lo que no
+    #se encuentra el boton de añadir nuevo contacto, devolviendo asi una exepcion "NoSuchElement". Si esto sucede:
+        repetidosCounter+=1;#aumentar contador de contactos repetidos
+        elementoNombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");#Volver a encontrar el input de nombre
+        elementoTelefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");#Volver a encontrar el input de telefono
 
-        continue;
+        elementoNombreDeContacto.clear();#Borrar el nombre del contacto repetido
+        elementoTelefonoContacto.clear();#Borrar el telefono del contacto repetido
 
-    nombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");
-    telefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");
+        continue; #Y pasar al siguiente contacto.
+
+    elementoNombreDeContacto = browser.find_element_by_xpath("//input[@name='nombre']");#Volver a encontrar el input de nombre
+    elementoTelefonoContacto = browser.find_element_by_xpath("//input[@name='telefono']");#Volver a encontrar el input de telefono
 
 
 print(f"El lote tuvo {repetidosCounter} numeros repetidos!");
--- addGroup.py
+++ addGroup.py
@@ -1,32 +1,119 @@
 from selenium import webdriver
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import pandas as pandas
+from sys import argv
+from os import environ
 
-loteExcel = pandas.read_excel('BASE 1 ARGENTINA.xlsx');
-columnaNombresDelLote = loteExcel['NOMBRE Y APELLIDO'];
+alertasUser = environ['alertasUser'];
+alertasPassword = environ['alertasPassword'];
 
-browser = webdriver.Firefox()
-browser.get('http://arwenance.sondeosglobal.com')
+#Inicializacion de variables necesarias
+url = "";
+nombreDeLote = "";
+nombreDeColumnaConNombres = "";
+nombreDeGrupo = "";
+codigoDeGrupo = 0;
+nombreDeColumnaConMensajes = "";
+#Inicializacion de variables necesarias
 
-usernameInput = browser.find_element_by_xpath("//input[@name='_username']")
-passwordInput = browser.find_element_by_xpath("//input[@name='_password']")
 
-usernameInput.send_keys("admin")
-passwordInput.send_keys("admin.123")
+#Asignacion de argumentos como valor de variables
+if argv[1]:
+    nombreDeLote = argv[1];
 
-browser.find_element_by_xpath("//button[@type='submit']").click()
-browser.find_element_by_xpath("//h4[text()='Gestion de Grupos']").click()
+if argv[2]:
+    nombreDeColumnaConNombres = argv[2];
 
-browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click()
+if argv[3]:
+    nombreDeColumnaConMensajes = argv[3];
 
-nombreDeGrupo = browser.find_element_by_xpath("//input[@name='nombre']")
-codigoDeGrupo = browser.find_element_by_xpath("//input[@name='codigo']")
-inputContactos = browser.find_element_by_xpath("//input[@placeholder='Selecciones contactos']")
+if argv[4]:
+    if argv[4] == 'uywenance':
+        url = 'http://uywenance.sondeosglobal.com/user/login';
+    elif argv[4] == 'arwenance':
+        url = 'http://arwenance.sondeosglobal.com/user/login';
+    elif argv[4] == 'argenpesos':
+        url = 'http://argenpesos.sondeosglobal.com/user/login';
+    else:
+        url = argv[4];
 
-nombreDeGrupo.send_keys("Test grupo random");
-codigoDeGrupo.send_keys("69420");
+if argv[5]:
+    nombreDeGrupo = argv[5];
+     
+if argv[6]:
+    codigoDeGrupo = argv[6];
+#Asignacion de argumentos como valor de variables
 
+
+#Pandas
+loteExcel = pandas.read_excel(f"./bases/{nombreDeLote}");
+columnaNombresDelLote = loteExcel[nombreDeColumnaConNombres];
+columnaMensajesDelLote = loteExcel[nombreDeColumnaConMensajes]
+#Pandas
+
+#Selenium init
+browser = webdriver.Firefox();
+browser.get(url);
+#Selenium init
+
+#Login
+usernameInput = browser.find_element_by_xpath("//input[@name='_username']");
+passwordInput = browser.find_element_by_xpath("//input[@name='_password']");
+
+#Credentials
+usernameInput.send_keys(alertasUser);
+passwordInput.send_keys(alertasPassword);
+#Credentials
+
+
+browser.find_element_by_xpath("//button[@type='submit']").click();
+#Login
+
+#Creating new group BEGINNING
+browser.find_element_by_xpath("//h4[text()='Gestion de Grupos']").click();
+
+browser.find_element_by_xpath("//a[@class='btn btn-primary btn-sm glyphicon glyphicon-plus cursor-pointer']").click();
+
+elementoNombreDeGrupo = browser.find_element_by_xpath("//input[@name='nombre']");
+elementoCodigoDeGrupo = browser.find_element_by_xpath("//input[@name='codigo']");
+inputContactos = browser.find_element_by_xpath("//input[@placeholder='Selecciones contactos']");
+
+elementoNombreDeGrupo.send_keys(nombreDeGrupo);
+elementoCodigoDeGrupo.send_keys(codigoDeGrupo);
+counterForConsoleLog = 0;
+counterForErrorConsoleLog = 0;
+
+print(f"Lote seleccionado: {nombreDeLote}");
+
+#Por cada nombre en el lote
 for nombre in columnaNombresDelLote:
 
-    print(inputContactos.send_keys(f"{nombre}"));
-    browser.find_element_by_xpath("//li[@class='select2-results__option select2-results__option--highlighted']").click();
+    #Intenta llenar la informacion pedida en la pagina con la informacion referente al nombre sobre el que se este iterando
+    try:
+        inputContactos.send_keys(f"{nombre}");
+        #TODO: encapsular esto en una funcion con un nombre descriptivo "escribirNombreDelContactoYSeleccionarlo()".
+        browser.find_element_by_xpath("//li[@class='select2-results__option select2-results__option--highlighted']").click();
+
+        #TODO: encapsular esto en una funcion con un nombre descriptivo "mostrarEnConsolaElLogDeContactoAñadidoAlInput()".
+        counterForConsoleLog+=1;
+        print(f"Cantidad de contactos añadidos al input: {counterForConsoleLog}!");
+    
+    #Si el usuario, por cualquier motivo no es encontrado por la pagina
+    except NoSuchElementException:
+        #Borrar el nombre del mismo
+        browser.find_element_by_xpath("//input[@class='select2-search__field']").clear();
+
+        #TODO: encapsular esto en una funcion con un nombre descriptivo "mostrarEnConsolaElLogDeContactoNoEncontrado()".
+        counterForErrorConsoleLog+=1;
+        print(f"Cantidad de contactos que no se encontraron: {counterForErrorConsoleLog}!");
+
+        #Y pasar al siguiente nombre
+        continue;
+
+
+#Ingresar mensaje al grupo
+browser.find_element_by_xpath("//input[@name='mensaje']").send_keys(columnaMensajesDelLote[0]);
+
+#Finalmente, guardar grupo con todos los contactos.
+browser.find_element_by_xpath("//input[@value='Guardar']").click();
